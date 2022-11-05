9944b84a96e66bf9c768555446831d52d8816cc6
Thorny128 <86695706+Thorny128@users.noreply.github.com>
2022-11-04 21:30:50 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+music1="Audio.m4a";
+music2="music.mp3";
+
+function preload(){
+    song=loadSound("Audio.m4a");
+    song=loadSound("music.mp3");
+}
+
+function setup(){
+    canvas=createCanvas(600, 500);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0, 0, 600, 500)
+}
